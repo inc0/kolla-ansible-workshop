8f6497e735978735980da88669d63c5d07bdd43c
Justin Scott <justin.a.scott@intel.com>
2017-05-01 21:53:24 +0000

    Add usage. Add timer and summary. Add vars os and user. Remove exit from loop.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
+if [ $# -eq 0 ]; then
+  echo "Usage: deploy.sh [action] [start] [end] [operating systen] "
+  echo "Example: deploy.sh create 0 10 centos - creates 11 instances of centos environment (i.e. stundent0 - student10)"
+  echo "Example: deploy.sh destroy 0 10 - destroys environments 0 - 10"
+  exit 1
+fi
 action=${1:-"create"}
 statedir="state"
-echo "====$(date)====$action" >> accounts.txt
-for i in `seq ${2:-0} ${3:-0}`;
+seq_start=${2:-0}
+seq_end=${3:-0}
+os=${4:-"ubuntu"}
+start=`date +%s`
+
+echo "====$(date)====$action $seq_start $seq_end $os" >> student-info
+for i in `seq $seq_start $seq_end`;
 do
   student="student$i"
   echo 
   if [ "x$action" = "xcreate" ]; then
     echo "Creating $student..."
-    password=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32})
-    terraform apply -state=$statedir/$student -var prefix=$student -var password=$password
-    echo "$student - $password" >> accounts.txt
-    exit 0
+    password=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32)
+    terraform apply -state=$statedir/$student -var prefix=$student -var password=$password -var os=$os -var user=$os
   fi
   if [ "x$action" = "xdestroy" ]; then
     echo "Destroying  $student..."
     terraform destroy -state=$statedir/$student -var prefix=$student -force
-    exit 0
   fi
-  echo "$1 is not a valid command"
 done
+end=`date +%s`
+echo "Students applied:  $seq_start - $seq_end"
+echo "Total time: $((end-start)) seconds"
